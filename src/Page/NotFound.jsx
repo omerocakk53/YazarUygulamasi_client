fe8e2c1dfd61a045ed3df551642c75cce6fb4be4
omerocakk53 <152520803+omerocakk53@users.noreply.github.com>
2025-02-04 13:49:38 +0300

    Delete src directory

--- src/Page/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NotFound() {
-  return (
-    <div style={{display:"flex",justifyContent:"center",marginTop:"150px",height:"100%",width:"100%"}}><h1>Not Found Page</h1></div>
-  )
-}
-
-export default NotFound
